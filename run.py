076ab6c8da1b534e2ac9b00a83b002db1dff970d
ofcRealXT <talhaozc.2010@gmail.com>
2026-01-12 20:33:31 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from app import create_app, db
+import os
 
 app = create_app()
 
-with app.app_context():
-    db.create_all()
+if os.getenv("RENDER") is None:
+    with app.app_context():
+        db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
